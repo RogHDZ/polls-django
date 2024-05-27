12255736b5067b55e60cae9e8b95b008641cd525
Rogelio <royhs23@gmail.com>
2024-05-27 10:12:01 -0600

    03 tercet commit

--- pollsapp2/polls/views.py
+++ pollsapp2/polls/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import Question
+
 # Create your views here.
+def home(request):
+    # return HttpResponse("welcome come")
+    questions = Question.objects.all()
+    return render(request, 'polls/home.html', {
+        "questions": questions
+    })
+
+def vote(request, q_id):
+    pass
+
+def results(request, q_id):
+    pass
